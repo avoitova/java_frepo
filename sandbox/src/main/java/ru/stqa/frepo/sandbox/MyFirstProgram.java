619ee489c87e8f50f22c3f1a2da85f133b2ef0a5
avoitova <aelita2012@gmail.com>
2021-01-30 15:16:00 +0200

    Update MyFirstProgram.java

--- sandbox/src/main/java/ru/stqa/frepo/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/frepo/sandbox/MyFirstProgram.java
@@ -3,6 +3,25 @@ package ru.stqa.frepo.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args){
-		System.out.println("Hello, world");
+		hello("world");
+		hello("Alex");
+		hello("Aelita");
+		double l = 5;
+		System.out.println("Area of the square with the side " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Area of the rectangle  with the sides " + a + " and " + b + " = " + area(a,b));
+	}
+
+	private static void hello(String somebody) {
+		System.out.println("Hello, " + somebody);
+	}
+
+	public static double area (double len){
+		return len*len;
+	}
+	public static double area(double a, double b){
+		return a * b;
 	}
 }
