e94653bcc88dbd975761ad1bb12d8e81a6f0c5c6
avoitova <aelita2012@gmail.com>
2021-01-31 12:47:59 +0200

    Добавление класса Point, Rectangle and Square

--- sandbox/src/main/java/ru/stqa/frepo/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/frepo/sandbox/MyFirstProgram.java
@@ -6,22 +6,34 @@ public class MyFirstProgram {
 		hello("world");
 		hello("Alex");
 		hello("Aelita");
-		double l = 5;
-		System.out.println("Area of the square with the side " + l + " = " + area(l));
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Area of the rectangle  with the sides " + a + " and " + b + " = " + area(a,b));
+		Square s = new Square(5);
+		System.out.println("Area of the square with the side " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Area of the rectangle  with the sides " + r.a + " and " + r.b + " = " + r.area());
+
+		/*Point p1 = new Point();
+		p1.x = 2;
+		p1.y = 2;
+		Point p2 = new Point();
+		p2.x = 4;
+		p2.y = 6;
+		System.out.println("Distance between two points " + "p1(" + p1.x + ";"+ p1.y +")" + " and "+ "p2(" + p2.x + ";"+ p2.y + ") is "+ distance(p1,p2));*/
+
+		Point p1 = new Point(2,2);
+		Point p2 = new Point (4,6);
+
+		System.out.println("Distance between two points " + "p1(" + p1.x + ";"+ p1.y +")" + " and "+ "p2(" + p2.x + ";"+ p2.y + ") is "+ p1.distance(p2));
+
 	}
 
 	private static void hello(String somebody) {
 		System.out.println("Hello, " + somebody);
 	}
+	/*public static double distance (Point p1, Point p2){
+		return Math.sqrt((p2.x- p1.x)*(p2.x- p1.x)+(p2.y- p1.y)* (p2.y- p1.y));
+	}*/
 
-	public static double area (double len){
-		return len*len;
-	}
-	public static double area(double a, double b){
-		return a * b;
-	}
+
 }
---
+++ sandbox/src/main/java/ru/stqa/frepo/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.frepo.sandbox;
+
+public class Point {
+
+  public double x;
+  public double y;
+  public Point (double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+  public double distance (Point p){
+    return Math.sqrt((p.x - x)*(p.x - x)+(p.y - y)* (p.y - y));
+  }
+
+}
