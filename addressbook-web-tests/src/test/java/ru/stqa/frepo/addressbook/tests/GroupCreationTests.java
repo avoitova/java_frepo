f8947b475de980a575f2e2ad769b9821812f493e
avoitova <aelita2012@gmail.com>
2021-02-19 21:37:41 +0200

    Modified tests by counting groups before and after test execution

--- addressbook-web-tests/src/test/java/ru/stqa/frepo/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frepo/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.frepo.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.frepo.addressbook.model.GroupData;
 
@@ -7,7 +8,11 @@ public class GroupCreationTests extends Testbase {
 
   @Test
   public void testGroupCreation() throws Exception {
+
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after,before+1);
   }
 }
