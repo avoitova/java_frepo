f43f46cd89dfde1b945a6e3a6a2d08f38111c025
avoitova <aelita2012@gmail.com>
2021-02-20 14:42:45 +0200

    refactoring max value search using lambda

--- addressbook-web-tests/src/test/java/ru/stqa/frepo/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frepo/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.frepo.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,16 +15,27 @@ public class GroupCreationTests extends Testbase {
 
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(),before.size() + 1);
-    int max = 0;
+   /* int max = 0;
     for (GroupData g: after){
       if (g.getId() > max){
         max = g.getId();
       }
-    }
+    }*/
+   /* Comparator<? super GroupData> byId = new Comparator<GroupData>() {
+      @Override
+      public int compare(GroupData o1, GroupData o2) {
+        return Integer.compare(o1.getId(), o2.getId());
+      }
+    };
+    int max = after.stream().max(byId).get().getId();*/
+
+
+
+    int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
     group.setId(max);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
