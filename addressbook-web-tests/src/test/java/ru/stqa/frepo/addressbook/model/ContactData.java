2774a8ae9b9b3fb4b54dc0b2a8ee8e6754d2a6a0
avoitova <aelita2012@gmail.com>
2021-03-06 22:50:41 +0200

    generator and data provider for contact data tests in xml and json

--- addressbook-web-tests/src/test/java/ru/stqa/frepo/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frepo/addressbook/model/ContactData.java
@@ -1,23 +1,44 @@
 package ru.stqa.frepo.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 
+@XStreamAlias("contact")
 public class ContactData {
-  private  int id = Integer.MAX_VALUE;;
+  @XStreamOmitField
+  private  int id = Integer.MAX_VALUE;
+  @Expose
   private  String firstname;
+  @Expose
   private  String lastname;
+  @Expose
   private  String company;
+  @Expose
   private  String address;
+  @Expose
   private  String mobile;
+  @Expose
   private  String email;
+  @Expose
   private  String email2;
+  @Expose
   private  String email3;
+  @Expose
   private String group;
+  @Expose
   private String home;
+  @Expose
   private String work;
+  @Expose
   private String allPhones;
+  @Expose
   private  String allEmails;
+  @Expose
   private String allAddress;
+  @Expose
   private File photo;
 
   public File getPhoto() {
--- addressbook-web-tests/src/test/java/ru/stqa/frepo/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frepo/addressbook/tests/ContactCreationTests.java
@@ -1,25 +1,65 @@
 package ru.stqa.frepo.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
 import org.hamcrest.MatcherAssert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.frepo.addressbook.model.ContactData;
 import ru.stqa.frepo.addressbook.model.Contacts;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends Testbase{
 
-  @Test
-  public void testContactCreation(){
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null){
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return  contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+  }
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null){
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());   //List<GroupData>.class
+    return  contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
+  }
+
+    /*File photo = new File("src/test/resources/stru.png");
+    list.add(new Object[] {new ContactData().withFirstname("Anna").withLastname("Petrova")
+            .withCompany("Luxoft").withAddress("Kyiv, Radyshcheva str. 10/14").withMobilePhone("0969365879")
+            .withEmail("apetrova@luxoft.com").withPhoto(photo).withGroup("test1")});*/
+
+
+
+  @Test(dataProvider = "validContactsFromXml")
+  public void testContactCreation(ContactData contact){
     app.goTo().goToHomePage();
-    Contacts before = app.getContactHelper().getContactSet();
     File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData().withFirstname("Anna")
-            .withLastname("Petrova").withCompany("Luxoft").withAddress("Kyiv, Radyshcheva str. 10/14")
-            .withMobilePhone("0969365879").withEmail("apetrova@luxoft.com").withPhoto(photo).withGroup("test1");
+    Contacts before = app.getContactHelper().getContactSet();
     app.getContactHelper().createContact(contact,true);
     Contacts after = app.getContactHelper().getContactSet();
     MatcherAssert.assertThat(after.size(), equalTo(before.size()+1));
